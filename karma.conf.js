21c07ebe5fd98688ac7abc618d6c0a96439ef414
TeerapongP <thirapongp7@gmail.com>
2024-12-31 17:03:36 +0700

    change action

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,24 @@
-module.exports = function(config) {
-    config.set({
-      basePath: '',
-      frameworks: ['jasmine'],
-      files: [
-        'src/**/*.spec.ts'
-      ],
-      preprocessors: {
-        'src/**/*.spec.ts': ['webpack']
+module.exports = function (config) {
+  config.set({
+    frameworks: ["jasmine"], // ใช้ Jasmine framework สำหรับ unit testing
+    files: [
+      "src/**/*.spec.ts", // รันไฟล์ spec ที่อยู่ในโฟลเดอร์ src
+    ],
+    preprocessors: {
+      "src/**/*.spec.ts": ["typescript"], // ใช้ TypeScript สำหรับไฟล์ spec
+    },
+    browsers: ["ChromeHeadless"], // ใช้ ChromeHeadless แทน ChromeHeadlessCI
+    singleRun: true, // ตั้งค่าให้รันแค่ครั้งเดียวแล้วปิด
+    reporters: ["progress", "kjhtml"], // กำหนดรูปแบบการแสดงผล
+    customLaunchers: {
+      ChromeHeadless: {
+        base: "ChromeHeadless",
+        flags: [
+          "--no-sandbox",
+          "--disable-gpu",
+          "--remote-debugging-port=9222",
+        ],
       },
-      reporters: ['progress'],
-      browsers: ['ChromeHeadlessCI'],  // ใช้ ChromeHeadless ใน CI
-      singleRun: true,  // ทดสอบครั้งเดียวแล้วหยุด
-      autoWatch: false, // ไม่ต้องคอยติดตามการเปลี่ยนแปลงของไฟล์
-      colors: true
-    });
-  };
-  
+    },
+  });
+};
